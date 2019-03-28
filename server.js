0f8c1ff3096991d3d37ab5d7568d9288544ac82a
ohsnaplol <emyb@bellsouth.net>
2019-03-27 22:57:23 -0400

    add login logout routes using firebase

--- server.js
+++ server.js
@@ -1,27 +1,61 @@
 const express = require('express')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
 const next = require('next')
 const bodyparser = require('body-parser')
+const admin = require('firebase-admin')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const firebase = admin.initializeApp(
+  {
+    credential: admin.credential.cert(require('./credentials/server')),
+    databaseURL: "https://dnd-tool-234621.firebaseio.com"
+  },
+  'server'
+)
+
 app.prepare().then(() => {
   const server = express()
   server.use(bodyparser.json())
+  server.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      saveUninitialized: true,
+      store: new FileStore({ path: '/tmp/sessions', secret: 'geheimnis' }),
+      resave: false,
+      rolling: true,
+      httpOnly: true,
+      cookie: { maxAge: 604800000 } // week
+    })
+  )
 
+  server.use((req, res, next) => {
+    req.firebaseServer = firebase
+    next()
+  })
 
   server.post('/login', (req, res) => {
-    return res.send('You did a login with ' + req.body.username)
+    if (!req.body) return res.sendStatus(400)
+
+    const token = req.body.token
+    firebase
+      .auth()
+      .verifyIdToken(token)
+      .then(decodedToken => {
+        req.session.decodedToken = decodedToken
+        return decodedToken
+      })
+      .then(decodedToken => res.json({ status: true, decodedToken }))
+      .catch(error => res.json({ error }))
   })
 
-  server.get('/b', (req, res) => {
-    return app.render(req, res, '/b', req.query)
-  })
-
-  server.get('/posts/:id', (req, res) => {
-    return app.render(req, res, '/posts', { id: req.params.id })
+  server.post('/api/logout', (req, res) => {
+    req.session.decodedToken = null
+    res.json({ status: true })
   })
 
   server.get('*', (req, res) => {
