9f3670716e0d29e3ba9b82cefe668733aafd98a5
ohsnaplol <emyb@bellsouth.net>
2019-04-07 00:36:05 -0400

    remove logout button and move it to layout

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,8 @@ import UserContext from '../components/UserContext'
 import firebase from '../components/firebase'
 
 export default class Index extends Component {
-  handleLogin (provider) {
+  handleLogin () {
     firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
-  }
-
-  handleLogout () {
-    firebase.auth().signOut()
   }
 
   render () {
@@ -21,11 +17,9 @@ export default class Index extends Component {
           Take your dungeon crawling to the next level 
           with real time maps, character data, and battle data!
         </p>
-        {user ? (
-          <button onClick={this.handleLogout}>Logout</button>
-        ) : (
+        {!user &&
           <button onClick={this.handleLogin}>Google Login</button>
-        )}
+        }
       </div>
     )
   }
