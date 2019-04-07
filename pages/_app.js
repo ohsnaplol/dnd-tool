69b8400e9c1186637297f11874cef1dac2007754
ohsnaplol <emyb@bellsouth.net>
2019-04-07 00:47:55 -0400

    added explanations in app

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,12 @@ import App, { Container } from 'next/app'
 import firebase from '../components/firebase'
 import Layout from '../components/Layout'
 
+/**
+ * This class wraps our entire web app. Checks for auth changes on Google's servers,
+ * and maintains this state in the MyApp state. It re-renders the whole app
+ * if a change is made to the Auth state. We make the user state aviailable
+ * globally with UserContext component which uses the React Context API.
+ */
 export default class MyApp extends App {
   static async getInitialProps ({ req, query }) {
     const user = req && req.session ? req.session.decodedToken : null
@@ -18,6 +24,12 @@ export default class MyApp extends App {
   }
 
   componentDidMount () {
+    /**
+     * This function listens for changes in auth state on Google's server, (logged in, logged out)
+     * If a user is found, set the user state to the user object. Submit their 
+     * token to our database in the POST request so we know if they're logged in on our database.
+     * If they're not logged in on Google's server, send a logout request to our server.
+     */
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
         this.setState({ user: user })
@@ -43,7 +55,9 @@ export default class MyApp extends App {
       }
     })
   }
-
+  /**
+   * Render is run when the component mounts, then every time the state or props are modified.
+   */
   render () {
     const { Component, pageProps } = this.props
     return (
