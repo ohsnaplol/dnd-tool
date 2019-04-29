a9a76deba382a3a2597f1e9c40b40adf8319a2be
ohsnaplol <emyb@bellsouth.net>
2019-04-28 23:30:34 -0400

    show list of games

--- pages/games.js
+++ pages/games.js
@@ -1,12 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import firebase from '../components/firebase'
 import UserContext from '../components/UserContext'
 
 function games(props) {
   const user = useContext(UserContext)
+  const [gamesList, setGamesList] = useState(undefined)
+  useEffect(() => {
+    firebase
+    .firestore()
+    .collection('/games')
+    .get()
+    .then(games => {
+      setGamesList(games.docs)
+    })
+  }, [])
 
   return (
     <div>
       <button>Create game</button>
+      {gamesList !== undefined && 
+        <ol>
+          {gamesList.map(doc => 
+            <li key={doc.id}>{doc.id}</li>
+          )}
+        </ol>
+      }
     </div>
   )
 }
