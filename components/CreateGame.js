d40cf22ab3ddba49971f78024d8185aa99f4d9a3
ohsnaplol <emyb@bellsouth.net>
2019-06-05 15:38:37 -0400

    create game is now a component

--- components/CreateGame.js
+++ components/CreateGame.js
@@ -1,28 +1,26 @@
-import React, {useState} from 'react'
+import React, {useState, useCallback} from 'react'
 import firebase from '../components/firebase'
-import Router from 'next/router'
 import Axios from 'axios';
 
 function create_game() {
   const [title, setTitle] = useState('')
 
-  function onCreateGameButtonPush() {
+  const onCreateGameButtonPush = useCallback(() =>
     firebase.auth().currentUser.getIdToken(true).then(idToken => {
       Axios.post('/api/game', {
         idToken,
         title,
       }).then(() => {
         alert('game created')
-        Router.push('/games')
       })
     }).catch(err => alert(err))
-  }
+  , [title])
 
   return (
     <div>
       <label htmlFor="game-input">Game title</label>
       <input placeholder="Game title" id="game-input" value={title} onChange={e => setTitle(e.target.value)}/>
-      <button onClick={() => onCreateGameButtonPush()}>Create Game</button>
+      <button onClick={onCreateGameButtonPush}>Create Game</button>
     </div>
   )
 }
